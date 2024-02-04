b2261fdae1301f76c41a2ba4b1bfa4846dec9a6e
Ricardo354 <yellow_richard@proton.me>
2024-02-04 20:59:39 -0300

    verbose_out() is more pretty now

--- pyrateFlix.py
+++ pyrateFlix.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+
 import sys
 import requests
 import argparse
-import pprint as p
+import libtorrent as lt
+
 arguments = {
 '-limit' : 'Limit the number of results per page',
 '-quality' : 'Filter by quality (480p, 720p, 1080p, 1080p.x265, 2160p, 3D)',
@@ -12,24 +14,32 @@ arguments = {
 '-sort_by' : 'Sort results by (title, year, rating, peers, seeds, download_count, like_count, date_added)',
 '-order_by' : 'Order results by ascending or descending (asc, desc)',
 '-with_rt_ratings' : 'Include Rotten Tomatoes rating (true, false)',
-'-verbose' : 'Display verbose output (title, id, imdb_code, lang)',
+'-verbose' : 'Display verbose output (title, id, imdb_code, lang, qualities)',
 
 }
 
-
+def fetch_info(json: dict, i: int):
+    title_long = json['data']['movies'][i]['title_long']
+    movie_id = f'id: {json["data"]["movies"][i]["id"]}'
+    movie_imdb = f'imbd_code: {json["data"]["movies"][i]["imdb_code"]}'
+    movie_lang = f'language: {json["data"]["movies"][i]["language"]}'
+    return [title_long, movie_id, movie_imdb, movie_lang]
 
 def verbose_out(json: dict, i: int):
+    info_list = fetch_info(json, i)
     qualities = set()
-    print(json['data']['movies'][i]['title_long'])
-    print(f'id: {json["data"]["movies"][i]["id"]}')
-    print(f'imbd_code: {json["data"]["movies"][i]["imdb_code"]}')
-    print(f'language: {json["data"]["movies"][i]["language"]}')
+
+    for i in range(len(info_list)):
+        print(info_list[i])
 
     '''
-    I COULD NOT FIGURE OUT A PRECISE AND EFFICIENT AND PRECISE WAY TO DO THIS, 
+    I COULD NOT FIGURE OUT A PRECISE AND EFFICIENT AND PRECISE WAY TO PRINT THE QUALITIES, 
     SO I JUST THREW IN A SET. IF A FUTURE USER/CONTRIBUTOR KNOWS A WAY TO ENHANCE
     THIS, FOR THE LOVE OF GOD MAKE THE COMMIT.
+
+    IF YOU USE AN ARRAY FOR THIS IT APPENDS 720p LIKE 20 TIMES, IT IS TERRIFYING.
     '''
+
     for movie in range(json['data']['movie_count']):
         for torrent in range(len(json['data']['movies'][movie]['torrents'])):
             qualities.add(json['data']['movies'][movie]['torrents'][torrent]['quality'])
@@ -38,8 +48,10 @@ def verbose_out(json: dict, i: int):
 
 
 
-def download_torrent(choice: int):
+def download_torrent(json: dict, choice: int):
     pass
+    # TORRENT_HASH = json['data']['movies']
+
 
 def remove_dash(text: str):
     return text.replace('-', '')
@@ -78,10 +90,32 @@ for i in range(GET['data']['movie_count']):
         print(GET['data']['movies'][i]['title_long'])
 
 print("What movie do you want to download?: ")
-choice = int(input())
+# choice = int(input())
 
-download_torrent(choice)
+# qu_choice = input()
+# if qu_choice not in 
 
+
+
+# download_torrent(choice)
+
+
+#TODO: ADD QUALITY CHOICE AFTER MOVIE CHOICE
+
+#TODO: FIX THIS
+'''
+./pyrateFlix.py -l 3 -q 'Toy Story'                                                               
+0 - Toy Story That Time Forgot (2014)
+1 - Toy Story of Terror (2013)
+2 - Charlie: A Toy Story (2012)
+3 - Traceback (most recent call last):
+  File "/home/johndoe/repos/pyrateFlix/./pyrateFlix.py", line 80, in <module>
+    print(GET['data']['movies'][i]['title_long'])
+          ~~~~~~~~~~~~~~~~~~~~~^^^
+IndexError: list index out of range
+'''
+
+#TODO: FIX -w
 
 
 #TODO: FINISH download_torrent fn
