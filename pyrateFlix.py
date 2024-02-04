1865c2340212f4a34abba919aa90a035117ca092
John Doe <yellow_richard@proton.me>
2024-02-04 14:34:21 -0300

    GOT THE QUALITY TO WORK IN -v WOOHOOOOOOOOOOOO

--- pyrateFlix.py
+++ pyrateFlix.py
@@ -2,7 +2,7 @@
 import sys
 import requests
 import argparse
-
+import pprint as p
 arguments = {
 '-limit' : 'Limit the number of results per page',
 '-quality' : 'Filter by quality (480p, 720p, 1080p, 1080p.x265, 2160p, 3D)',
@@ -15,23 +15,28 @@ arguments = {
 '-verbose' : 'Display verbose output (title, id, imdb_code, lang)',
 
 }
-'''
-#TODO: ADD VERBOSE ARGUEMENT FOR ENABLING
-MOVIE1
-    title_long
-    id
-    imdb_code
-    language
-OR 
-TITLE_LONG
 
-'''
+
 
 def verbose_out(json: dict, i: int):
+    qualities = set()
     print(json['data']['movies'][i]['title_long'])
-    print(f'id: {json['data']['movies'][i]['id']}')
-    print(f'imbd_code: {json['data']['movies'][i]['imdb_code']}')
-    print(f'language: {json['data']['movies'][i]['language']}\n')
+    print(f'id: {json["data"]["movies"][i]["id"]}')
+    print(f'imbd_code: {json["data"]["movies"][i]["imdb_code"]}')
+    print(f'language: {json["data"]["movies"][i]["language"]}')
+
+    '''
+    I COULD NOT FIGURE OUT A PRECISE AND EFFICIENT AND PRECISE WAY TO DO THIS, 
+    SO I JUST THREW IN A SET. IF A FUTURE USER/CONTRIBUTOR KNOWS A WAY TO ENHANCE
+    THIS, FOR THE LOVE OF GOD MAKE THE COMMIT.
+    '''
+    for movie in range(json['data']['movie_count']):
+        for torrent in range(len(json['data']['movies'][movie]['torrents'])):
+            qualities.add(json['data']['movies'][movie]['torrents'][torrent]['quality'])
+
+    print(f'{qualities}\n')
+
+
 
 def download_torrent(choice: int):
     pass
@@ -76,4 +81,12 @@ print("What movie do you want to download?: ")
 choice = int(input())
 
 download_torrent(choice)
+
+
+
+#TODO: FINISH download_torrent fn
+#TODO: SPECIFY PATH TO DOWNLOAD
+
+#TODO: DESKTOP NOTIFICATION WITH OPEN FOLDER BUTTON
+    
 
