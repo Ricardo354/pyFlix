1fad277032cef9db14af6271377e9a23d65ba964
John Doe <yellow_richard@proton.me>
2024-02-03 21:14:58 -0300

    Got verbose output to work

--- pyrateFlix.py
+++ pyrateFlix.py
@@ -11,7 +11,8 @@ arguments = {
 '-genre' : 'Filter by genre (See http://www.imdb.com/genre/ for full list)',
 '-sort_by' : 'Sort results by (title, year, rating, peers, seeds, download_count, like_count, date_added)',
 '-order_by' : 'Order results by ascending or descending (asc, desc)',
-'-with_rt_ratings' : 'Include Rotten Tomatoes rating (true, false)'
+'-with_rt_ratings' : 'Include Rotten Tomatoes rating (true, false)',
+'-verbose' : 'Display verbose output (title, id, imdb_code, lang)',
 
 }
 '''
@@ -26,11 +27,17 @@ TITLE_LONG
 
 '''
 
-def download_torrent():
+def verbose_out(json: dict, i: int):
+    print(json['data']['movies'][i]['title_long'])
+    print(f'id: {json['data']['movies'][i]['id']}')
+    print(f'imbd_code: {json['data']['movies'][i]['imdb_code']}')
+    print(f'language: {json['data']['movies'][i]['language']}\n')
 
+def download_torrent(choice: int):
+    pass
 
-def remove_dash(argument):
-    return argument.replace('-', '')
+def remove_dash(text: str):
+    return text.replace('-', '')
 
 parser = argparse.ArgumentParser(prog='pyrateFlix')
 
@@ -39,10 +46,14 @@ url = 'https://yts.mx/api/v2/list_movies.json?'
 for arg_name, arg_desc in arguments.items():
     if arg_name == '-quality':
         parser.add_argument(f'-qu', f'-{arg_name}', help=f'{arg_desc}')
+    elif arg_name == '-verbose':
+        parser.add_argument(f'-{arg_name[1]}', f'-{arg_name}', help=f'{arg_desc}', action='store_true')
     else:
         parser.add_argument(f'-{arg_name[1]}', f'-{arg_name}', help=f'{arg_desc}')
+        
 
 args = parser.parse_args()
+
 
 for arg_name, arg_desc in arguments.items():
     arg_value = getattr(args, remove_dash(arg_name))
@@ -52,16 +63,17 @@ for arg_name, arg_desc in arguments.items():
         else:
             url += f'&{remove_dash(arg_name)}={arg_value}'
 
-data = requests.get(url).json()
-print(data['data']['movie_count'])
-for i in range(data['movie_count']):
-    print(data['data']['movies'][i]['title_long'])
+GET = requests.get(url).json()
 
-#TODO: ENUMERATE MOVIES LIKE
-'''
-1 - MOVIE1
-2 - MOVIE2...
-WHAT MOVIE DO YOU WANT TO DOWNLOAD?
-1
-DOWNLOAD TORRENT...
-'''
+for i in range(GET['data']['movie_count']):
+    print(f'{i} - ', end='')
+    if args.verbose:
+        verbose_out(GET, i)
+    else:
+        print(GET['data']['movies'][i]['title_long'])
+
+print("What movie do you want to download?: ")
+choice = int(input())
+
+download_torrent(choice)
+
