1f4eb5595f0624a866bca8005efb5a43f080c9d4
Ricardo354 <yellow_richard@proton.me>
2024-02-05 11:09:53 -0300

    This commit will now possibilitate the user to see the progress of his download, not through a progress bar, but through numbers, also , fetch_info() now returns qualities

--- pyrateFlix.py
+++ pyrateFlix.py
@@ -7,7 +7,6 @@ import urllib
 import libtorrent as lt
 import time 
 import warnings
-import progressbar
 
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -31,17 +30,7 @@ def fetch_info(json: dict, i: int):
     movie_id = f'id: {json["data"]["movies"][i]["id"]}'
     movie_imdb = f'imbd_code: {json["data"]["movies"][i]["imdb_code"]}'
     movie_lang = f'language: {json["data"]["movies"][i]["language"]}'
-
-
-    return [title_long, movie_id, movie_imdb, movie_lang]
-
-def verbose_out(json: dict, i: int):
-    info_list = fetch_info(json, i)
     qualities = set()
-
-    for i in range(len(info_list)):
-        print(info_list[i])
-
     '''
     I COULD NOT FIGURE OUT A PRECISE AND EFFICIENT AND PRECISE WAY TO PRINT THE QUALITIES, 
     SO I JUST THREW IN A SET. IF A FUTURE USER/CONTRIBUTOR KNOWS A WAY TO ENHANCE
@@ -49,12 +38,18 @@ def verbose_out(json: dict, i: int):
 
     IF YOU USE AN ARRAY FOR THIS IT APPENDS 720p LIKE 20 TIMES, IT IS TERRIFYING.
     '''
-
     for movie in range(json['data']['movie_count']):
         for torrent in range(len(json['data']['movies'][movie]['torrents'])):
             qualities.add(json['data']['movies'][movie]['torrents'][torrent]['quality'])
 
-    print(f'{qualities}\n')
+
+    return [title_long, movie_id, movie_imdb, movie_lang, qualities]
+
+def verbose_out(json: dict, i: int):
+    info_list = fetch_info(json, i)
+
+    for i in range(len(info_list)):
+        print(info_list[i])
 
 
 
@@ -65,25 +60,19 @@ def download_torrent(json: dict, choice: int, quality_choice: str, path: str):
             break
     magnet_link = f'magnet:?xt=urn:btih:{TORRENT_HASH}&dn={urllib.parse.quote(fetch_info(json, choice)[0])}&tr=http://tracker.opentrackr.org:1337/announce&tr=udp://tracker.openbittorrent.com:80'
 
-    ses = lt.session()
 
+    ses = lt.session()
     params = {
         'save_path': path  
     }
     h = lt.add_magnet_uri(ses, magnet_link, params)
 
-     
-    try:
-        widgets = [f'Downloading: {fetch_info(json, choice)[0]} ', progressbar.AnimatedMarker()]
-        bar = progressbar.ProgressBar(widgets=widgets).start()
-        while not h.status().is_seeding:
-            time.sleep(1)
-            bar.update(0.6)
-    except ModuleNotFoundError:
-        print("Downloading...")
-        while not h.status().is_seeding:
-            time.sleep(1)
 
+    while (not h.status().is_seeding):
+        print('\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s...' % (
+            h.status().progress * 100, h.status().download_rate / 1000, h.status().upload_rate / 1000,
+            h.status().num_peers, h.status().state), end='')
+        time.sleep(1)
         
 
 def remove_dash(text: str):
@@ -120,7 +109,7 @@ GET = requests.get(url).json()
 n = int(GET['data']['movie_count'] if not args.limit else args.limit)
 
 for i in range(n):
-    print(f'{i} - ', end='')
+    print(f'\n{i} - ', end='')
     if args.verbose:
         verbose_out(GET, i)
     else:
@@ -129,7 +118,7 @@ for i in range(n):
 
 
 choice = int(input("What movie do you want to download?: "))
-PATH = input("Do you want to choose a path to download your torrents?: (default .)")
+PATH = input("Do you want to choose a path to download your torrents?: ")
 
 while True:
     quality_choice = input("Enter the quality you want to download (e.g., 720p): ")
@@ -139,10 +128,12 @@ while True:
     else:
         print("Invalid quality choice. Please choose from the available qualities.")
 
-#TODO: PROGRESS BAR;
 
 #TODO: ENABLE MULTIPLE TORRENT DOWNLOADING.
+# -- > make magnet links and then append to a list , the download each one. 
 
 #TODO: DESKTOP NOTIFICATION BUTTON
 
 #TODO: FIX -w -- > UPDATE; JUST CANT FUCKING FIND IT , GOING TO LEAVE THIS TODO HERE ANYWAY
+
+#TODO: QUALITY NOW PRINTS WITHOUT -v
